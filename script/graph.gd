df969113b9e1f3cdb9e77bb80ea91defc276a7d8
instr3 <instr3@163.com>
2021-03-17 10:51:38 +0800

    Rewrote decorator import

--- script/graph.gd
+++ script/graph.gd
@@ -70,77 +70,78 @@ func __get_raw_element_center(puzzle, raw_element, element_type, id):
 			center += puzzle.vertices[int(raw_face_node)].pos
 		return center / len(raw_element['Nodes']['_arr'])
 		
-func add_element(puzzle, raw_element, element_type, id=-1):
+func __find_decorator(raw_element, xsi_type):
 	if ('Decorator' in raw_element):
 		var raw_decorator = raw_element['Decorator']
-		if (raw_decorator['xsi:type'] == "ThreeWayPuzzleDecorator"):
-			puzzle.n_ways = 3
-			puzzle.symmetry_type = 0
-			puzzle.symmetry_center = __get_raw_element_center(puzzle, raw_element, element_type, id)
-			puzzle.symmetry_center += Vector2(float(raw_decorator['DeltaX']), float(raw_decorator['DeltaY']))
-			puzzle.solution_colors.push_back(ColorN(raw_decorator['SecondLineColor']))
-			puzzle.solution_colors.push_back(ColorN(raw_decorator['ThirdLineColor']))
+		if (raw_decorator['xsi:type'] == xsi_type):
+			raw_decorator['__consumed'] = true
+			return raw_decorator
+	return null
+	
+func add_element(puzzle, raw_element, element_type, id=-1):
+	var symmetry_decorator = __find_decorator(raw_element, "ThreeWayPuzzleDecorator")
+	if (symmetry_decorator):
+		puzzle.n_ways = 3
+		puzzle.symmetry_type = 0
+		puzzle.symmetry_center = __get_raw_element_center(puzzle, raw_element, element_type, id)
+		puzzle.symmetry_center += Vector2(float(symmetry_decorator['DeltaX']), float(symmetry_decorator['DeltaY']))
+		puzzle.solution_colors.push_back(ColorN(symmetry_decorator['SecondLineColor']))
+		puzzle.solution_colors.push_back(ColorN(symmetry_decorator['ThirdLineColor']))
 	if (element_type == 1):
 		var v1 = int(raw_element['Start'])
 		var v2 = int(raw_element['End'])
 		var p1 = puzzle.vertices[v1].pos
 		var p2 = puzzle.vertices[v2].pos
-		if ('Decorator' in raw_element):
-			var raw_decorator = raw_element['Decorator']
-			if (raw_decorator['xsi:type'] == "BrokenDecorator"):
-				var p3 = p1 * 0.8 + p2 * 0.2
-				var p4 = p1 * 0.2 + p2 * 0.8
-				var v3 = push_vertex_vec(puzzle, p3)
-				var v4 = push_vertex_vec(puzzle, p4)
-				puzzle.vertices[v3].decorator = load('res://script/decorators/broken_decorator.gd').new()
-				puzzle.vertices[v3].decorator.direction = (p2 - p1).normalized()
-				puzzle.vertices[v4].decorator = puzzle.vertices[v3].decorator
-				push_edge_idx(puzzle, v1, v3)
-				push_edge_idx(puzzle, v2, v4)
-				return
+		if (__find_decorator(raw_element, "BrokenDecorator")):
+			var p3 = p1 * 0.8 + p2 * 0.2
+			var p4 = p1 * 0.2 + p2 * 0.8
+			var v3 = push_vertex_vec(puzzle, p3)
+			var v4 = push_vertex_vec(puzzle, p4)
+			puzzle.vertices[v3].decorator = load('res://script/decorators/broken_decorator.gd').new()
+			puzzle.vertices[v3].decorator.direction = (p2 - p1).normalized()
+			puzzle.vertices[v4].decorator = puzzle.vertices[v3].decorator
+			push_edge_idx(puzzle, v1, v3)
+			push_edge_idx(puzzle, v2, v4)
+			return
 		var v3 = push_vertex_vec(puzzle, p1 * 0.5 + p2 * 0.5)
 		push_edge_idx(puzzle, v1, v3)
 		push_edge_idx(puzzle, v2, v3)
-		if ('Decorator' in raw_element):
-			var raw_decorator = raw_element['Decorator']
-			if (raw_decorator['xsi:type'] == "PointDecorator"):
-				puzzle.vertices[v3].decorator = load('res://script/decorators/point_decorator.gd').new()
-				puzzle.vertices[v3].decorator.color = ColorN(raw_decorator['Color'])
-			else:
-				print('Unsupported decorator: %s on edge' % raw_decorator['xsi:type'])
+		var point_decorator = __find_decorator(raw_element, "PointDecorator")
+		if (point_decorator):
+			puzzle.vertices[v3].decorator = load('res://script/decorators/point_decorator.gd').new()
+			puzzle.vertices[v3].decorator.color = ColorN(point_decorator['Color'])
 	elif (element_type == 2):
 		var facet_vertices = []
 		for raw_face_node in raw_element['Nodes']['_arr']:
 			facet_vertices.push_back(puzzle.vertices[int(raw_face_node)])
 		var facet = Facet.new(facet_vertices)
 		puzzle.facets.push_back(facet)
-		if ('Decorator' in raw_element):
-			var raw_decorator = raw_element['Decorator']
-			if (raw_decorator['xsi:type'] == "TriangleDecorator"):
-				facet.decorator = load('res://script/decorators/triangle_decorator.gd').new()
-				facet.decorator.color = ColorN(raw_decorator['Color'])
-				facet.decorator.count = int(raw_decorator['Count'])
-			elif (raw_decorator['xsi:type'] == "StarDecorator"):
-				facet.decorator = load('res://script/decorators/star_decorator.gd').new()
-				facet.decorator.color = ColorN(raw_decorator['Color'])
-			else:
-				print('Unsupported decorator: %s on facet' % raw_decorator['xsi:type'])
+		var triangle_decorator = __find_decorator(raw_element, "TriangleDecorator")
+		if (triangle_decorator):
+			facet.decorator = load('res://script/decorators/triangle_decorator.gd').new()
+			facet.decorator.color = ColorN(triangle_decorator['Color'])
+			facet.decorator.count = int(triangle_decorator['Count'])
+		var star_decorator = __find_decorator(raw_element, "StarDecorator")
+		if (star_decorator):
+			facet.decorator = load('res://script/decorators/star_decorator.gd').new()
+			facet.decorator.color = ColorN(star_decorator['Color'])
 	if (element_type == 0):
-		if ('Decorator' in raw_element):
-			var raw_decorator = raw_element['Decorator']
-			if (raw_decorator['xsi:type'] == "StartDecorator"):
-				puzzle.vertices[id].decorator = load('res://script/decorators/start_decorator.gd').new()
-			elif (raw_decorator['xsi:type'] == "EndDecorator"):
-				var end_length = float(raw_decorator['Length'])
-				var end_angle = deg2rad(float(raw_decorator['Angle']))
-				var p_end = puzzle.vertices[id].pos + Vector2(cos(end_angle), sin(end_angle)) * end_length
-				var v_end = push_vertex_vec(puzzle, p_end)
-				push_edge_idx(puzzle, id, v_end)
-				puzzle.vertices[v_end].decorator = load('res://script/decorators/end_decorator.gd').new()
-			else:
-				print('Unsupported decorator: %s on vertex' % raw_decorator['xsi:type'])
+		if (__find_decorator(raw_element, "StartDecorator")):
+			puzzle.vertices[id].decorator = load('res://script/decorators/start_decorator.gd').new()
+		var end_decorator = __find_decorator(raw_element, "EndDecorator")
+		if (end_decorator):
+			var end_length = float(end_decorator['Length'])
+			var end_angle = deg2rad(float(end_decorator['Angle']))
+			var p_end = puzzle.vertices[id].pos + Vector2(cos(end_angle), sin(end_angle)) * end_length
+			var v_end = push_vertex_vec(puzzle, p_end)
+			push_edge_idx(puzzle, id, v_end)
+			puzzle.vertices[v_end].decorator = load('res://script/decorators/end_decorator.gd').new()
 	
-	
+	if ('Decorator' in raw_element):
+		var raw_decorator = raw_element['Decorator']
+		if (!raw_decorator['__consumed']):
+			print('Unsupported decorator: %s on %s' % [raw_decorator['xsi:type'], ['node', 'edge', 'facet'][element_type]])
+
 	
 func load_from_xml(file):
 	var puzzle = Puzzle.new()
