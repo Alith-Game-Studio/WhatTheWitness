1ae407f499ec06db153c3d2c8b7d15147006cbbd
instr3 <instr3@163.com>
2021-05-17 18:37:09 +0800

    Reimplemented symmetry and self-collision detection

--- script/solution.gd
+++ script/solution.gd
@@ -37,25 +37,80 @@ class DiscreteSolutionState:
 			if (len(vertices[MAIN_WAY]) >= 2):
 				return main_way_vertex_id == vertices[MAIN_WAY][-2]
 			return false
+		assert(false)
 		
-	func transist(puzzle, new_vertex_ids):
+	func transist(puzzle, main_way_vertex_id):
+		var limit = 1.0 + 1e-6
+		var main_way_pos = puzzle.vertices[main_way_vertex_id].pos
 		if (solution_stage == SOLUTION_STAGE_EXTENSION):
 			var new_state = copy()
+			var main_way_dir = (main_way_pos - puzzle.vertices[vertices[MAIN_WAY][-1]].pos).normalized()
 			for way in range(puzzle.n_ways):
-				new_state.vertices[way].push_back(new_vertex_ids[way])
-			return new_state
+				var way_vertex_id
+				if (way == MAIN_WAY):
+					way_vertex_id = main_way_vertex_id
+				else:
+					var way_crossroad_vertex_id = vertices[way][-1]
+					var way_dir = get_symmetry_vector(puzzle, way, main_way_dir)
+					way_vertex_id = -1
+					for edge in puzzle.edges:
+						var new_vertex_id
+						var edge_dir
+						if (edge.start.index == way_crossroad_vertex_id):
+							new_vertex_id = edge.end.index
+							edge_dir = edge.end.pos - edge.start.pos
+						elif (edge.end.index == way_crossroad_vertex_id):
+							new_vertex_id = edge.start.index
+							edge_dir = edge.start.pos - edge.end.pos
+						else:
+							continue
+						edge_dir = edge_dir.normalized()
+						if ((edge_dir - way_dir).length() < 1e-6):
+							way_vertex_id = new_vertex_id
+							break
+				if (way_vertex_id == -1):
+					return [null, null]
+				new_state.vertices[way].push_back(way_vertex_id)
+			var main_edge_length = (puzzle.vertices[new_state.vertices[MAIN_WAY][-1]].pos - puzzle.vertices[new_state.vertices[MAIN_WAY][-2]].pos).length()
+			var occupied_vertices = {}
+			var endpoint_occupied = 0
+			for way in range(puzzle.n_ways):
+				var edge_length = (puzzle.vertices[new_state.vertices[way][-1]].pos - puzzle.vertices[new_state.vertices[way][-2]].pos).length()
+				if (abs(edge_length - main_edge_length) > 1e-3):
+					limit = min(limit, 1.0 - 1e-6)
+					if (edge_length < main_edge_length):
+						limit = min(limit, 1.0 * edge_length / main_edge_length)
+				for i in range(len(new_state.vertices[way])):
+					if (i == len(new_state.vertices[way]) - 1):
+						if (new_state.vertices[way][i] in occupied_vertices):
+							endpoint_occupied = max(endpoint_occupied, occupied_vertices[new_state.vertices[way][i]])
+					occupied_vertices[new_state.vertices[way][i]] = 2 if i == 0 else 1
+				if (endpoint_occupied == 1): # colliding with other lines / self-colliding
+					limit = min(limit, 1.0 - puzzle.line_width / edge_length)
+				elif (endpoint_occupied == 2): # colliding with start points
+					limit = min(limit, 1.0 - (puzzle.start_size + puzzle.line_width / 2) / edge_length)
+			return [new_state, limit]
+		assert(false)
 	
 	func get_symmetry_point(puzzle, way, pos):
 		if (way == 0):
 			return pos
 		if (puzzle.symmetry_type == Graph.SYMMETRY_ROTATIONAL):
 			return (pos - puzzle.symmetry_center).rotated(2 * PI * way / puzzle.n_ways) + puzzle.symmetry_center
+		assert(false)
 		
 	func get_symmetry_vector(puzzle, way, vec):
 		if (way == 0):
 			return vec
 		if (puzzle.symmetry_type == Graph.SYMMETRY_ROTATIONAL):
 			return vec.rotated(2 * PI * way / puzzle.n_ways)
+		assert(false)
+			
+	func pos_to_vertex_id(puzzle, pos, eps=1e-3):
+		for vertex in puzzle.vertices:
+			if (vertex.pos.distance_to(pos) < eps):
+				return vertex.index
+		return -1
 		
 	func get_nearest_start(puzzle, pos):
 		var best_dist = puzzle.start_size
@@ -86,6 +141,7 @@ class SolutionLine:
 	var started: bool
 	var state_stack: Array
 	var progress: float
+	var limit: float
 	var validity = 0
 	var vertices_occupied: Array
 	
@@ -154,9 +210,15 @@ class SolutionLine:
 				if (state_stack[-1].is_retraction(puzzle, vertex_id)):
 					progress = 1.0 - 1e-6
 				else:
-					var new_state = state_stack[-1].transist(puzzle, [vertex_id])
-					state_stack.push_back(new_state)
-					progress = 1e-6
+					var new_state_with_limit = state_stack[-1].transist(puzzle, vertex_id)
+					var new_state = new_state_with_limit[0]
+					var new_limit = new_state_with_limit[1]
+					if (new_state != null):
+						state_stack.push_back(new_state)
+						limit = new_limit
+						progress = 1e-6
+					else:
+						progress = 1.0 - 1e-6
 			else:
 				return
 		if (len(state_stack) > 1):
@@ -165,8 +227,6 @@ class SolutionLine:
 			var edge_vec = v1.pos - v2.pos
 			var edge_length = edge_vec.length()
 			
-			# calculate upper limit (lower limit is always 0)
-			var limit = 1.0 + 1e-6
 			# calculate new progress
 			var projected_length = edge_vec.normalized().dot(delta) / edge_length
 			var projected_det = abs(det(edge_vec.normalized(), delta)) / edge_length
@@ -183,6 +243,7 @@ class SolutionLine:
 				projected_progress -= projected_det * 0.5 # discorage
 			if (projected_progress <= 0.0):
 				state_stack.pop_back()
+				limit = 1.0 + 1e-6
 				progress = 1.0 - 1e-6
 				return
 			if (projected_progress >= limit):
