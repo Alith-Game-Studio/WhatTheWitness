b069422a3f91b04184bddbf3f3f7ca57e0653a1c
instr3 <instr3@163.com>
2021-05-25 19:28:18 +0800

    Modified CSP helper

--- script/judgers/csp_helper.gd
+++ script/judgers/csp_helper.gd
@@ -2,18 +2,22 @@ extends Node
 
 var binomial = null
 const BINOMIAL_LIMIT = 100000000
+
+func initialize():
+	binomial = []
+	for k in range(1000):
+		binomial.append([])
+		for l in range(k + 1):
+			if (l == 0 or l == k):
+				binomial[k].append(1.0)
+			else:
+				var result = binomial[k - 1][l] + binomial[k - 1][l - 1]
+				result = min(result, BINOMIAL_LIMIT + k)
+				binomial[k].append(result)
+	
 func get_binomial(i, j):
 	if (binomial == null):
-		binomial = []
-		for k in range(1000):
-			binomial.append([])
-			for l in range(k + 1):
-				if (l == 0 or l == k):
-					binomial[k].append(1.0)
-				else:
-					var result = binomial[k - 1][l] + binomial[k - 1][l - 1]
-					result = min(result, BINOMIAL_LIMIT + k)
-					binomial[k].append(result)
+		initialize()
 	if (j > i or j < 0):
 		return 0
 	if (j == 0 or j == i):
