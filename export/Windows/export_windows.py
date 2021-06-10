939ab7d7c4379d622c9780e8cad9835e23866da8
instr3 <instr3@163.com>
2021-06-10 14:36:46 +0800

    Added export script

---
+++ export/Windows/export_windows.py
@@ -0,0 +1,34 @@
+import os
+import shutil
+EDITOR_PATH = R'D:\Programming\WitnessVisualizer\WitnessVisualizer\bin\Release'
+BUILD_FOLDER = '_build'
+BUILD_ZIP = 'build.zip'
+
+IGNORE_EXTENSIONS = ['.zip', '.rar', '.docx', '.pdb', '.config']
+
+def copy_editor():
+    shutil.copytree(EDITOR_PATH, BUILD_FOLDER)
+    os.rename(os.path.join(BUILD_FOLDER, 'WitnessVisualizer.exe'), os.path.join(BUILD_FOLDER, 'Editor.exe'))
+    shutil.rmtree(os.path.join(BUILD_FOLDER, 'Puzzles'))
+    for file in os.listdir(BUILD_FOLDER):
+        base, ext = os.path.splitext(file)
+        if (ext.lower() in IGNORE_EXTENSIONS):
+            os.unlink(os.path.join(BUILD_FOLDER, file))
+        if (file.lower() == 'templateGenerator.exe'):
+            os.unlink(os.path.join(BUILD_FOLDER, file))
+
+if __name__ == '__main__':
+    try:
+        shutil.rmtree(BUILD_FOLDER)
+    except:
+        pass
+    copy_editor()
+    shutil.copytree('../../puzzles', os.path.join(BUILD_FOLDER, 'puzzles'))
+    shutil.copyfile('Custom Witness Puzzles.exe', os.path.join(BUILD_FOLDER, 'Game.exe'))
+    shutil.copyfile('Custom Witness Puzzles.pck', os.path.join(BUILD_FOLDER, 'Game.pck'))
+    try:
+        os.unlink(BUILD_ZIP)
+    except:
+        pass
+    shutil.make_archive(os.path.splitext(BUILD_ZIP)[0], 'zip', BUILD_FOLDER)
+    shutil.rmtree(BUILD_FOLDER)
