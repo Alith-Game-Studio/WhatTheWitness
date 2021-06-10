2fc0c3aa3b069953daef6c067b7d7eefd3ecde9c
instr3 <instr3@163.com>
2021-06-10 10:10:12 +0800

    Improved ghost visualization

--- script/decorators/ghost_decorator.gd
+++ script/decorators/ghost_decorator.gd
@@ -4,17 +4,18 @@ var rule = 'ghost'
 var pattern: int
 
 func draw_shape(canvas, puzzle, color):
+	var multiplier = 1 if pattern == 0 else -1
 	var radius = 0.425 * puzzle.line_width
 	var tail_height = 0.5 * radius
 	var points_arc = []
 	var nb_points = 16
 	for i in range(nb_points + 1):
 		var angle_point = i * PI / nb_points
-		points_arc.push_back(Vector2(cos(angle_point), sin(angle_point)) * radius)
+		points_arc.push_back(Vector2(cos(angle_point), sin(angle_point)) * radius * multiplier)
 	for j in range(7):
 		var x = (j / 3.0 - 1) * radius
 		var y = -radius + j % 2 * tail_height
-		points_arc.push_back(Vector2(x, y))
+		points_arc.push_back(Vector2(x, y) * multiplier)
 	canvas.add_polygon(points_arc, color)
 	
 func draw_foreground(canvas: Visualizer.PuzzleCanvas, owner, owner_type: int, puzzle: Graph.Puzzle):
--- script/decorators/ghost_manager.gd
+++ script/decorators/ghost_manager.gd
@@ -12,63 +12,70 @@ func draw_below_solution(canvas, owner, owner_type, puzzle, solution):
 	var vertex_ghost_property = state.event_properties[id]
 	var main_way = Solution.MAIN_WAY
 	var vertices_main_way = state.vertices[main_way]
-	for layer in range(3):
-		for way in range(puzzle.n_ways):
-			if (way >= len(state.vertices)):
-				continue
-			if (len(state.solution_stage) <= way or state.solution_stage[way] != Solution.SOLUTION_STAGE_GHOST):
-				continue
-			var vertices_way = state.vertices[way]
-			var color = puzzle.solution_colors[way]
-			if (solution.validity == -1):
-				color = Color.black
-			elif (solution.validity == 0): # drawing illumination
-				color = Color(1 - (1 - color.r) * 0.6, 1 - (1 - color.g) * 0.6, 1 - (1 - color.b) * 0.6, color.a)
-			var delta_shift = Vector2.ZERO
-			var last_pos = puzzle.vertices[vertices_way[0]].pos
-			if (len(state.solution_stage) > way and state.solution_stage[way] == Solution.SOLUTION_STAGE_SNAKE):
-				if (len(vertices_way) > 1 and !puzzle.vertices[vertices_way[-1]].is_puzzle_end):
-					last_pos = last_pos * (1 - solution.progress) + puzzle.vertices[vertices_way[1]].pos * solution.progress
-			canvas.add_circle(last_pos, puzzle.start_size, color)
-			var last_point_ghosted = false
-			var line_color = puzzle.line_color
-			var ghosted_color = Color(color.r * 0.1 + line_color.r * 0.9, color.g * 0.1 + line_color.g * 0.9, color.b * 0.1 + line_color.b * 0.9)
-			for i in range(1, len(vertices_way)):
-				var segment = [puzzle.vertices[vertices_way[i - 1]].pos, puzzle.vertices[vertices_way[i]].pos]
-				var percentage
-				if (i + 1 == len(vertices_way)):
-					var segment_main = [puzzle.vertices[vertices_main_way[-2]].pos, puzzle.vertices[vertices_main_way[-1]].pos]
-					var main_line_length = (segment_main[1] - segment_main[0]).length()
-					var line_length = (segment[1] - segment[0]).length()
-					var segment_progress = solution.progress
-					percentage = segment_progress * main_line_length / line_length
-				else:
-					percentage = 1.0
-				var pos = segment[0] * (1.0 - percentage) + segment[1] * percentage
-				var point_ghosted = is_solution_point_ghosted(vertex_ghost_property, way, i)
-				if (int(point_ghosted) + int(last_point_ghosted) == 2 - layer):
-					if (last_pos != null):
-						var point_color = ghosted_color if point_ghosted else color
-						var prev_point_color = ghosted_color if last_point_ghosted else color
-						if (percentage < 1 - GHOST_COLOR_INTERP_START):
-							var t = max(0, (percentage - GHOST_COLOR_INTERP_START) / (1 - 2 * GHOST_COLOR_INTERP_START))
-							point_color = Color(point_color.r * t + prev_point_color.r * (1 - t),
-								point_color.g * t + prev_point_color.g * (1 - t),
-								point_color.b * t + prev_point_color.b * (1 - t))
+	for way in range(puzzle.n_ways):
+		if (way >= len(state.vertices)):
+			continue
+		if (len(state.solution_stage) <= way or state.solution_stage[way] != Solution.SOLUTION_STAGE_GHOST):
+			continue
+		var vertices_way = state.vertices[way]
+		var color = puzzle.solution_colors[way]
+		if (solution.validity == -1):
+			color = Color.black
+		elif (solution.validity == 0): # drawing illumination
+			color = Color(1 - (1 - color.r) * 0.6, 1 - (1 - color.g) * 0.6, 1 - (1 - color.b) * 0.6, color.a)
+		var delta_shift = Vector2.ZERO
+		var last_pos = puzzle.vertices[vertices_way[0]].pos
+		canvas.add_circle(last_pos, puzzle.start_size, color)
+		var last_point_ghosted = false
+		var line_color = puzzle.line_color
+		var percentage
+		for i in range(1, len(vertices_way)):
+			var segment = [puzzle.vertices[vertices_way[i - 1]].pos, puzzle.vertices[vertices_way[i]].pos]
 
-						var t1 = min(percentage, GHOST_COLOR_INTERP_START) / percentage
-						var t2 = min(percentage, 1 - GHOST_COLOR_INTERP_START) / percentage
-						canvas.add_circle(pos, puzzle.line_width / 2.0, point_color)
-						canvas.add_gradient_lines(
-							[last_pos, last_pos * (1 - t1) + pos * t1,
-							last_pos * (1 - t2) + pos * t2, pos],
-							puzzle.line_width, [prev_point_color, prev_point_color, point_color, point_color])
-				last_pos = pos
-				last_point_ghosted = point_ghosted
-			if (layer == 2):
-				if (solution.validity == 0): # drawing
-					canvas.add_circle(last_pos, puzzle.line_width / 2.7, line_color)
-					canvas.add_circle(last_pos, puzzle.line_width / 3.1, color)
+			if (i + 1 == len(vertices_way)):
+				var segment_main = [puzzle.vertices[vertices_main_way[-2]].pos, puzzle.vertices[vertices_main_way[-1]].pos]
+				var main_line_length = (segment_main[1] - segment_main[0]).length()
+				var line_length = (segment[1] - segment[0]).length()
+				var segment_progress = solution.progress
+				percentage = segment_progress * main_line_length / line_length
+			else:
+				percentage = 1.0
+			var pos = segment[0] * (1.0 - percentage) + segment[1] * percentage
+			var point_ghosted = is_solution_point_ghosted(vertex_ghost_property, way, i)
+			if (last_pos != null):
+				var a = 0 if point_ghosted else 1
+				var prev_a = 0 if last_point_ghosted else 1
+				if (percentage < 1 - GHOST_COLOR_INTERP_START):
+					var t = max(0, (percentage - GHOST_COLOR_INTERP_START) / (1 - 2 * GHOST_COLOR_INTERP_START))
+					a = a * t + prev_a * (1 - t)
+				var point_color = Color(color.r, color.g, color.b, a)
+				var prev_point_color = Color(color.r, color.g, color.b, prev_a)
+
+				var t1 = min(percentage, GHOST_COLOR_INTERP_START) / percentage
+				var t2 = min(percentage, 1 - GHOST_COLOR_INTERP_START) / percentage
+				canvas.add_circle(pos, puzzle.line_width / 2.0, point_color)
+				canvas.add_gradient_lines(
+					[last_pos, last_pos * (1 - t1) + pos * t1,
+					last_pos * (1 - t2) + pos * t2, pos],
+					puzzle.line_width, [prev_point_color, prev_point_color, point_color, point_color])
+			last_pos = pos
+			last_point_ghosted = point_ghosted
+		if (solution.validity == 0): # drawing indicators for invisible line
+			canvas.add_circle(last_pos, puzzle.line_width / 2.7, line_color)
+			canvas.add_circle(last_pos, puzzle.line_width / 3.1, color)
+			if (len(vertices_way) >= 3):
+				var last_v_pos = puzzle.vertices[vertices_main_way[-1]].pos
+				var second_last_v_pos = puzzle.vertices[vertices_main_way[-2]].pos
+				var third_last_v_pos = puzzle.vertices[vertices_main_way[-3]].pos
+				var last_distance = last_v_pos.distance_to(second_last_v_pos) * percentage
+				var second_last_distance = second_last_v_pos.distance_to(third_last_v_pos)
+				var follow_distance = 0.5 # the distance that the second indicator from the first indicator
+				var second_percentage = (last_distance + second_last_distance - follow_distance) / second_last_distance
+				second_percentage = clamp(second_percentage, 0.0, 1.0)
+				var second_indicator_pos = second_percentage * second_last_v_pos + (1 - second_percentage) * third_last_v_pos
+				canvas.add_circle(second_indicator_pos, puzzle.line_width / 3.4, line_color)
+				canvas.add_circle(second_indicator_pos, puzzle.line_width / 3.8, color)
+				
 func is_solution_point_ghosted(vertex_ghost_property, way, id):
 	if (len(vertex_ghost_property) <= way):
 		return false
