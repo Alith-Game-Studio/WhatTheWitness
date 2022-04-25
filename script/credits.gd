ef82e14004b451961da74528443ede9ebbc3e6dd
instr3 <instr3@163.com>
2022-04-25 12:02:47 +0800

    Merge WitCup10 into WTW

--- script/credits.gd
+++ script/credits.gd
@@ -5,11 +5,12 @@ const FULL_AUTHORS = {
 	'witness': 'from The Witness',
 	'wc1': 'from the 1st WitCup (by Gentova)',
 	'wc2': 'from the 2nd WitCup (by Gentova and Zhvsjia)',
-	'wc3': 'from the 3rd WitCup (by Miaoji et al.)',
+	'wc3': 'from the 3rd WitCup (by Fluittene et al.)',
 	'wc4': 'from the 4th WitCup (by Artless)',
 	'wc5': 'from the 5th WitCup (by Alith)',
 	'wc6': 'from the 6th WitCup (by Zhvsjia)',
 	'wc7': 'from the 7th WitCup (by Alith)',
+	'wc10': 'from the 10th WitCup (by Artless, Leaving Leaves, Zhvsjia, 牙刷架, 普通的解谜人, sevenkplus & Fluittene)',
 	'rpg': 'from the Witness RPG Mod (by Sigma144)',
 }
 
